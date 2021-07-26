978644a4957b23f73ab34bbb501a3cbea448d2e3
Rashid Majeed <rashid.majeed@systemsltd.com>
2021-07-26 17:13:23 +0500

    remove amir

--- src/com/systems/ltd/Testing.java
+++ src/com/systems/ltd/Testing.java
@@ -5,7 +5,7 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Testing sample project amir ");
+		System.out.println("Testing sample project");
 
 	}
 
