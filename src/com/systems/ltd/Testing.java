4762e69ef48da840aa92d8b88a2263532fcadcf9
Rashid Majeed <rashid.majeed@systemsltd.com>
2021-07-26 17:05:03 +0500

    added amir name

---
+++ src/com/systems/ltd/Testing.java
@@ -0,0 +1,12 @@
+package com.systems.ltd;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing sample project amir ");
+
+	}
+
+}
