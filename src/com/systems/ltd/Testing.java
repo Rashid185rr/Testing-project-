13120d05a96986fb8b1f34cdc5ca3da52f5506b3
Rashid Majeed <rashid.majeed@systemsltd.com>
2021-07-26 17:25:46 +0500

    aded master

--- src/com/systems/ltd/Testing.java
+++ src/com/systems/ltd/Testing.java
@@ -5,7 +5,7 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Testing sample project");
+		System.out.println("Testing sample project master");
 
 	}
 
